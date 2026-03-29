faf4c6666f18d560322de2a485b74a8dc60b5a08
Sekouba CAMARA <sekoubacamara100@gmail.com>
2026-03-29 17:04:15 -0400

    added the routes in the server file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import 'dotenv/config'
 import express from 'express'
 import './config/db.js'
+import bookRoutes from './routes/api/bookRoutes.js'
+import userRoutes from './routes/api/userRoutes.js'
 
 const app = express()
 
@@ -8,9 +10,15 @@ const port = process.env.PORT || 8081
 
 app.use(express.json())
 
-app.get('/', (req, res) =>{
-    res.send('Hello world')
-})
+// make-believe frontend (login page)
+app.get('/', (req, res) => res.send('<a href="/api/users/auth/github"><button>Login with GitHub</button></a>'))
+ 
+// make-believe frontend (success page)
+app.get('/success', (req, res) => res.send('<h1>Success!</h1><a href="/">Back</a>'))
+
+
+app.use('/api/notes', bookRoutes);
+app.use('/api/users', userRoutes)
 
 app.listen(port, () => {
     console.log('Server is listening on port: localhost:', port)
